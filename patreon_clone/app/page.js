40817e7300ded7f7b88d2db5dbc68ba3456e510d
Rohit <rohitkakralia94@gmail.com>
2024-11-07 00:45:39 +0530

    start

--- patreon_clone/app/page.js
+++ patreon_clone/app/page.js
@@ -2,6 +2,30 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>HomePage</div>
+    <>
+    
+    <div className=" flex justify-center gap-4 flex-col items-center text-white h-[44vh]">
+      <div className="font-bold flex gap-3 text-3xl">Welcome To Patreon </div>
+      <p>A Crowdfundong plateform for creaters. Get funded by your fans and followers.</p>
+      <div>
+      <button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Start Here</button>
+      <button type="button" class="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Read More</button>
+
+      </div>
+    </div>
+    <div>
+      <div className="bg-white h-1 opacity-10"></div>
+    </div>
+
+    <div className="text-white container mx-auto">
+        <h1 className="text-2xl text-center font-bold my-14">Your Fans can Donate you here!</h1>
+        <div className="flex justify-around gap-5">
+          <div>
+            <img src="" alt="" />
+            <p></p>
+          </div>
+        </div>
+    </div>
+    </>
   );
 }
--- patreon_clone/components/Footer.js
+++ patreon_clone/components/Footer.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer>
-        Footer
+    <footer className='bg-blue-950 text-white flex justify-center items-center px-4 h-16'>
+        <div className='text-center'>Copyright &copy; All right reserved</div>
     </footer>
   )
 }
